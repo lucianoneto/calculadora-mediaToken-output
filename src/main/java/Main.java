7e492a6c642e8f6b52a094ae38c631b3dc967caa
Gustavo <gustavo.borges.guimaraes100@gmail.com>
2022-09-16 09:53:27 -0300

    Atualizações feitas no código e nos nomes de variáveis.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,36 +8,40 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Digite a quantidade de linhas de cada bloco: \n");
-        int linhas = scanner.nextInt();
+
+        int tamanhoBloco = scanner.nextInt();
         int cont = 0;
-        Double somatorio1 = Double.valueOf(0);
-        Double somatorio2 = Double.valueOf(0);
-        Double resultado1;
-        List<Double> coluna4 = new ArrayList<>();
+
+        double iExperimental1 = 0;
+        double iExperimental2 = 0;
+        double resultadoFinal;
+
+        List<Double> resultados = new ArrayList<>();
 
         Scanner scannerNew = new Scanner(new FileReader("arquivo_yosef2.txt"));
         while (scannerNew.hasNextLine()) {
-            String line = scannerNew.nextLine();
-            line = line.trim().replaceAll("\\s+", "¥");
+            String linhaAtual = scannerNew.nextLine();
+            linhaAtual = linhaAtual.trim().replaceAll("\\s+", "¥");
 
             if (cont != 0) {
-                String[] linha = line.split("¥");
+                String[] linha = linhaAtual.split("¥");
 
-                somatorio1 += Double.parseDouble(linha[3]);
-                somatorio2 += Double.parseDouble(linha[4]);
+                iExperimental1 += Double.parseDouble(linha[3]);
+                iExperimental2 += Double.parseDouble(linha[4]);
 
-                if (cont == linhas) {
+                if (cont == tamanhoBloco) {
                     cont = 0;
-                    resultado1 = (Math.pow((somatorio1 - somatorio2), 2) / ((Math.pow(somatorio1, 2) + Math.pow(somatorio2,2))));
-                    coluna4.add(resultado1);
-                    somatorio1 = Double.valueOf(0);
+                    resultadoFinal = (Math.pow((iExperimental1 - iExperimental2), 2) / ((Math.pow(iExperimental1, 2) + Math.pow(iExperimental2, 2))));
+                    resultados.add(resultadoFinal);
+                    iExperimental1 = 0;
                 }
             }
             cont++;
         }
-        for(int i = 0; i < coluna4.size(); i++) {
-            System.out.println("BLOCO " + (i+1) );
-            System.out.println("Somatorio : " + coluna4.get(i));
+
+        for (int i = 1; i < resultados.size(); i++) {
+            System.out.println("BLOCO " + i);
+            System.out.println("Somatorio : " + resultados.get(i));
         }
     }
 }
