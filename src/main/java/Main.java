037b46a4ee8da5549187f20f9242040ea18860a9
Gustavo <gustavo.borges.guimaraes100@gmail.com>
2022-09-16 09:53:54 -0300

    Atualizações feitas no código e nos nomes de variáveis.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,7 +10,7 @@ public class Main {
         System.out.println("Digite a quantidade de linhas de cada bloco: \n");
 
         int tamanhoBloco = scanner.nextInt();
-        int cont = 0;
+        int contador = 0;
 
         double iExperimental1 = 0;
         double iExperimental2 = 0;
@@ -23,20 +23,20 @@ public class Main {
             String linhaAtual = scannerNew.nextLine();
             linhaAtual = linhaAtual.trim().replaceAll("\\s+", "¥");
 
-            if (cont != 0) {
+            if (contador != 0) {
                 String[] linha = linhaAtual.split("¥");
 
                 iExperimental1 += Double.parseDouble(linha[3]);
                 iExperimental2 += Double.parseDouble(linha[4]);
 
-                if (cont == tamanhoBloco) {
-                    cont = 0;
+                if (contador == tamanhoBloco) {
+                    contador = 0;
                     resultadoFinal = (Math.pow((iExperimental1 - iExperimental2), 2) / ((Math.pow(iExperimental1, 2) + Math.pow(iExperimental2, 2))));
                     resultados.add(resultadoFinal);
                     iExperimental1 = 0;
                 }
             }
-            cont++;
+            contador++;
         }
 
         for (int i = 1; i < resultados.size(); i++) {
