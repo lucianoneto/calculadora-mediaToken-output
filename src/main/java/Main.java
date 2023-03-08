ed155c0a0ea8d9a383f1050ebb055a61e6ab9ca7
lucianoneto <lucianoneto1033@gmail.com>
2023-03-08 09:45:04 -0300

    first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,31 +1,53 @@
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
+    private static final int TOKEN_T5 = 2;
+
     public static void main(String[] args) throws IOException {
+        if (args.length < 2) {
+            System.err.println("Error: 2 arguments are needed, file name and number of starting tokens.");
+            return;
+        }
 
-        String nameFile = args[0];
+        String fileName = args[0];
+        int initialTokenCount;
+        try {
+            initialTokenCount = Integer.parseInt(args[1]);
+        } catch (NumberFormatException e) {
+            System.err.println("Error: Invalid number of starting tokens.");
+            return;
+        }
 
-        int executionsNumber = 0;
-        float timeValue = 0.0f;
+        int outputTokenCount = 0;
+        int valuePi2 = 0;
+        int valuePe2 = 0;
 
-        try (Scanner scannerNew = new Scanner(new FileReader(nameFile))) {
-            while (scannerNew.hasNextLine()) {
-                String currentLine = scannerNew.nextLine();
+        try (Scanner scanner = new Scanner(new FileReader(fileName))) {
+            while (scanner.hasNextLine()) {
+                String currentLine = scanner.nextLine();
 
-                if (!currentLine.isBlank()) {
-                    String[] line = currentLine.trim().split("\\s+");
-                    if (Objects.equals(line[1], "time....:")) {
-                        executionsNumber++;
-                        timeValue += Float.parseFloat(line[2]);
-                    }
+                if (currentLine.isBlank())
+                    continue;
+
+                String[] line = currentLine.trim().split("\\s+");
+
+                if (line.length <= TOKEN_T5 || !line[TOKEN_T5].equals("t5"))
+                    continue;
+
+                line = scanner.nextLine().trim().split("\\s");
+                valuePi2 = Integer.parseInt(line[3]);
+                line = scanner.nextLine().trim().split("\\s");
+                valuePe2 = Integer.parseInt(line[3]);
+                if (valuePi2 != valuePe2) {
+                    outputTokenCount++;
                 }
             }
         }
 
-        float average = (executionsNumber != 0) ? (timeValue / executionsNumber) : 0.0f;
-        System.out.println(average);
+        int loss = initialTokenCount - outputTokenCount;
+        System.out.println("Loss: " + loss);
+        System.out.println("Gain: " + outputTokenCount);
     }
 }
